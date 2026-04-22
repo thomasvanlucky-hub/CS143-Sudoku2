3bb8fa719f36605cea2db5f4031b861d62733081
ThomasPK <thomasvanlucky@gmail.com>
2026-04-22 11:55:27 -0700

    Incomplete change to the isValid method

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -11,43 +11,86 @@ public class SudokuBoard {
    public SudokuBoard(String fileName) {
       this();
       try {
-        Scanner console = new Scanner(new File(fileName));
-        for(int r = 0; r < board[r].length; r++){
+         Scanner console = new Scanner(new File(fileName));
+         for(int r = 0; r < board.length; r++){
             if(console.hasNext()) {
-            String line = console.next();
-            for(int c = 0; c < board[0].length; c++) {
-               char value = line.charAt(c);
-               if(value == '.') {
-                  board[r][c] = 0;
-               } else {
-                  board[r][c] = value - '0';
-                 }
+               String line = console.next();
+               for(int c = 0; c < board[0].length; c++) {
+                  char value = line.charAt(c);
+                  if(value == '.') {
+                     board[r][c] = 0;
+                  } else {
+                     board[r][c] = value - '0';
+                  }
+               }
+            }
+         }
+      } catch (FileNotFoundException e) {
+         System.out.println("Error: File not found.");
+      }
+   }   
+   
+   public boolean isValid() {
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[0].length; c++) {
+            if(board[r][c] > board.length) {               // 1st part
+               return false;
             }
          }
       }
-    } catch (FileNotFoundException e) {
-        System.out.println("Error: File not found.");
-    }
-}   
+      
+      for(int c = 0; c < board.length; c++) {
+         
+      }
+      
+      return true;
+   }
+   
+   private boolean checkNumbers() {
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[0].length; c++) {
+            if(board[r][c] < 0 || board[r][c] > 9) {
+            return false;
+            }
+         }
+      }
+      
+      return true;
+   }
+   
+   private boolean checkRow() {
+      Set<Integer> set = new HashSet<>();
+   
+      for(int r = 0; r < board.length; r++) {
+         set.add(r);
+         if(set.contains(r)) {
+            return false;
+         } 
+      }
+      
+      
+      return true;
+   }
+   
    public String toString() {
       String result = "";
       String line = "+-------+-------+-------+\n";
-
-      for (int r = 0; r < 9; r++) {
+   
+      for (int r = 0; r < board.length; r++) {
          if (r % 3 == 0) {
             result = result + line;
          }
-         for (int c = 0; c < 9; c++) {
+         for (int c = 0; c < board[0].length; c++) {
             if (c % 3 == 0) {
                result = result + "| ";
             }
             if (board[r][c] == 0) {
-                result = result + "- "; 
+               result = result + "- "; 
             } else {
-                result = result + board[r][c] + " ";
+               result = result + board[r][c] + " ";
             }
          }
-        result = result + "|\n";
+         result = result + "|\n";
       }
       result = result + line;
       return result;
